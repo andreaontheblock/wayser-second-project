129c26f3090129cc951e0db9287b85efa08f6021
Andrea Moya <andrea@MacBook-Pro-de-Andrea.local>
2018-07-13 10:49:21 +0200

    changed all the var for cobst

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
