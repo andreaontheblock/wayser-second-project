0b887813d035f7a30cd9210f8fcf0c064fe147c2
Andrea Moya <andrea@MacBook-Pro-de-Andrea.local>
2018-07-13 20:15:37 +0200

    initiated function to look for a user by service category

--- routes/services.js
+++ routes/services.js
@@ -3,12 +3,18 @@
 const express = require('express');
 const router = express.Router();
 
+const Service = require('../models/service');
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/:params', function (req, res, next) {
+router.get('/:category', function (req, res, next) {
+  const serviceCategory = req.params.category;
+
+  Service.find; // UWU buscar users by category e.g. 'technology'
+
   res.render('services-category');
 });
 
