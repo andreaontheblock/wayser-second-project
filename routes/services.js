5aca5bea7be7b11625c46978641ea0d991bfc379
Montse Badia <mbadia_11@hotmail.com>
2018-07-15 08:36:56 +0200

    retrieved username in list of services

--- routes/services.js
+++ routes/services.js
@@ -14,9 +14,9 @@ router.get('/:category', function (req, res, next) {
   const serviceCategory = req.params.category;
   const correctServiceCategory = serviceCategory.charAt(0).toUpperCase() + serviceCategory.substr(1); // UWU SUPER GREAT CODE IN ONE FUKIN LINE!!
 
-  Service.find({category: correctServiceCategory}) // UWU buscar users by category e.g. 'technology'
-    .then((users) => {
-      res.render('services-category', {users: users});
+  Service.find({category: correctServiceCategory}).populate('provider') // UWU buscar users by category e.g. 'technology'
+    .then((services) => {
+      res.render('services-category', {services: services});
     })
     .catch(next);
 });
