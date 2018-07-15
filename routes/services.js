58873543cb505d83206dee10869c03113e88ddef
Montse Badia <mbadia_11@hotmail.com>
2018-07-15 16:40:00 +0200

    fixed route to social-services and health-care

--- routes/services.js
+++ routes/services.js
@@ -12,7 +12,14 @@ router.get('/', function (req, res, next) {
 
 router.get('/:category', function (req, res, next) {
   const serviceCategory = req.params.category;
-  const correctServiceCategory = serviceCategory.charAt(0).toUpperCase() + serviceCategory.substr(1); // UWU SUPER GREAT CODE IN ONE FUKIN LINE!!
+  let correctServiceCategory = serviceCategory.charAt(0).toUpperCase() + serviceCategory.substr(1); // UWU SUPER GREAT CODE IN ONE FUKIN LINE!!
+  if (correctServiceCategory.includes('-')) {
+    correctServiceCategory = correctServiceCategory.replace(/-/, ' ');
+    let array = correctServiceCategory.split(' ');
+    let arrayUpperCased = array[1].charAt(0).toUpperCase() + array[1].substr(1);
+    array[1] = arrayUpperCased;
+    correctServiceCategory = array.join(' ');
+  }
 
   Service.find({category: correctServiceCategory}).populate('provider') // UWU buscar users by category e.g. 'technology'
     .then((services) => {
