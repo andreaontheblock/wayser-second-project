d2b5e2acf7efd0407236e0dbd9166babb02695f9
Montse Badia <mbadia_11@hotmail.com>
2018-07-16 12:06:40 +0200

    fixed middleware is id valid

--- middlewares/isIdValid.js
+++ middlewares/isIdValid.js
@@ -1,7 +1,7 @@
 const ObjectId = require('mongoose').Types.ObjectId; // UWU explicar Andrea
 
 function isIdValid (req, res, next) {
-  if (!ObjectId.isValid(req.params.id)) {
+  if (!ObjectId.isValid(req.params.serviceId)) {
     res.status(404);
     return res.render('not-found');
   }
