b8731f2f6e231b956d3ee45449191917a59dfe3b
Montse Badia <mbadia_11@hotmail.com>
2018-07-17 16:43:32 +0200

    added leaflet and mapbox to display map on service details

---
+++ public/javascripts/map.js
@@ -0,0 +1,8 @@
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.streets',
+  accessToken: 'pk.eyJ1IjoibW9udHNlMDMiLCJhIjoiY2pqcHN5NHk3M2djeDNrcXM5aTFmYWI2eSJ9.BYUUW6YUVADssrZJ47vueA'
+}).addTo(mymap);
