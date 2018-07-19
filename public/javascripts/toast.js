80e890f62a2582b57a0e29269dd797990da1a892
Montse Badia <mbadia_11@hotmail.com>
2018-07-19 14:45:31 +0200

    fixed toast and improved css on profile view

--- public/javascripts/toast.js
+++ public/javascripts/toast.js
@@ -1,9 +1,12 @@
 function main () {
-  var c = document.getElementById('email-submit');
-  console.log(c);
+  // var c = document.getElementById('email-submit');
+  // // window.service;
+  // // var serviceProvider = windows;
+  // // console.log(serviceProvider);
+  // console.log(c);
   document.getElementById('email-submit').addEventListener('click', function () {
     console.log('hello');
-    toastr.info('Email sent successfuly');
+    toastr.info('Email sent successfully');
   });
 }
 
